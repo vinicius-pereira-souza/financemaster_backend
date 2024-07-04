280830f70572d77f3a661517556521e694608052
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2024-07-04 16:02:31 -0300

    chore: multer configurado

---
+++ src/middlewares/uploadImage..ts
@@ -0,0 +1,24 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/users");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const imageUpload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg)$/)) {
+      // upload only png and jpg format
+      return cb(new Error("Por favor, envie apenas png ou jpg!"));
+    }
+    cb(null, true);
+  },
+});
+
+export { imageUpload };
